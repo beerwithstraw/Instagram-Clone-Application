00a381b360678076a9d3ae253047f37707e06aef
beerwithstraw <agarwalp@tcd.ie>
2021-02-03 00:28:59 +0000

    Post Header Icons

--- assets/components/PostHeader.js
+++ assets/components/PostHeader.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/Entypo'
 
 import ProfilePicture from './ProfilePicture'
 const PostHeader = ({ imageUri, name }) => {
     return (
         <View style={styles.container}>
-            <ProfilePicture uri={imageUri} size={30}/>
-            <Text style={styles.text}>{name}</Text>
+            <View style={styles.left}>
+                <ProfilePicture uri={imageUri} size={30}/>
+                <Text style={styles.text}>{name}</Text>
+            </View>
+
+            <View style={styles.right}>
+                <Icon name="dots-three-vertical" size={20} />
+            </View>
 
         </View>
     )
@@ -17,6 +24,15 @@ export default PostHeader
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: -5
+    },
+    left: {
+        flexDirection: 'row'
+    },
+    right: {
+        marginRight: 10
     },
     text: {
         alignSelf: 'center',
