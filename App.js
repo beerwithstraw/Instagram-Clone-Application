b148c33b0d497ba5a291476d2429a06f43608f84
beerwithstraw <agarwalp@tcd.ie>
2021-02-03 01:17:12 +0000

    Imported Icons for Bottom Navigation

--- App.js
+++ App.js
@@ -6,8 +6,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createStackNavigator } from '@react-navigation/stack';
 
+import Foundation from "react-native-vector-icons/Foundation";
+import Feather from "react-native-vector-icons/Feather";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+
 import HomeScreen from './screens/HomeScreen';
 import DiscoveryScreen from './screens/DiscoveryScreen';
+import PostScreen from './screens/PostScreen';
+import NotificationsScreen from './screens/NotificationsScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
 
 
 
@@ -25,12 +35,37 @@ export default function App() {
       {/* <Stack.Navigator screenOptions={globalScreenOptions}>
         <Stack.Screen name='Home'/>
       </Stack.Navigator> */}
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            if (route.name === 'Home') {
+              return <Foundation name="home" size={size} color={color} />;
+            }
+            if (route.name === 'Discovery') {
+              return <Feather name="search" size={size} color={color} />;
+            }
+            if (route.name === 'Post') {
+              return <Feather name="plus-square" size={size} color={color} />;
+            }
+            if (route.name === 'Notifications') {
+              return <AntDesign name="hearto" size={size} color={color} />;
+            }
+            if (route.name === 'Profile') {
+              return <Ionicons name="person-outline" size={size} color={color} />;
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#000',
+          inactiveTintColor: 'gray',
+          showLabel: false,
+        }}
+      >
         <Tab.Screen name='Home' component={HomeScreen} />
         <Tab.Screen name='Discovery' component={DiscoveryScreen} />
-        <Tab.Screen name='Post' component={HomeScreen} />
-        <Tab.Screen name='Notifications' component={HomeScreen} />
-        <Tab.Screen name='Profile' component={HomeScreen} />
+        <Tab.Screen name='Post' component={PostScreen} />
+        <Tab.Screen name='Notifications' component={NotificationsScreen} />
+        <Tab.Screen name='Profile' component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
 
