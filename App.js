8e6020dcd73e69129aaec4639875475b9b80d851
beerwithstraw <agarwalp@tcd.ie>
2021-02-03 02:19:32 +0000

    App Header Stack

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createStackNavigator } from '@react-navigation/stack';
@@ -10,7 +10,10 @@ import Foundation from "react-native-vector-icons/Foundation";
 import Feather from "react-native-vector-icons/Feather";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import IoniconsIcon from 'react-native-vector-icons/Ionicons'
 
+import Logo from './assets/Instagram-Logo.png'
+import IGBlack from './assets/IG-Black.png' 
 
 import HomeScreen from './screens/HomeScreen';
 import DiscoveryScreen from './screens/DiscoveryScreen';
@@ -18,23 +21,42 @@ import PostScreen from './screens/PostScreen';
 import NotificationsScreen from './screens/NotificationsScreen';
 import ProfileScreen from './screens/ProfileScreen';
 
-
-
-
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator(); 
+
+function HomeStackScreen () {
+  return (
+    <Stack.Navigator screenOptions={globalScreenOptions}>
+      <Stack.Screen name='Home' component={HomeScreen}/>
+    </Stack.Navigator>
+  )
+}
 const globalScreenOptions = {
-  headerStyle: {backgroundColor: "#2C6BED"},
-  headerTitleScreen: {color: "white"},
-  headerTintColor: "white"
+  title: "Instagram",
+  headerStyle: {backgroundColor: "white"},
+  headerTitleScreen: {color: "black"},
+  headerTintColor: "black",
+  headerTitle: () => (
+    <Image source={Logo} resizeMode="contain" style={{width: 120, height:80 }}/>
+  ),
+  headerLeft: () => (
+    <Feather name="camera" size={25} color={'#000'} styles={{
+      
+    }}/>
+  ),
+  headerLeftContainerStyle: {
+    marginLeft: 15,
+  },
+  headerRight: () => (
+    <IoniconsIcon name="paper-plane-outline" size={25} color="#000"/>
+  ),
+  headerRightContainerStyle: {
+    marginRight: 15,
+  },
 }
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
-      {/* <Stack.Navigator screenOptions={globalScreenOptions}>
-        <Stack.Screen name='Home'/>
-      </Stack.Navigator> */}
       <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
@@ -61,7 +83,7 @@ export default function App() {
           showLabel: false,
         }}
       >
-        <Tab.Screen name='Home' component={HomeScreen} />
+        <Tab.Screen name='Home' component={HomeStackScreen} />
         <Tab.Screen name='Discovery' component={DiscoveryScreen} />
         <Tab.Screen name='Post' component={PostScreen} />
         <Tab.Screen name='Notifications' component={NotificationsScreen} />
